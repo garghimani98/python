3b4cbc779205d4d8976db40fc32b283907be6fda
garghimani98 <garghimani98@gmail.com>
2021-09-06 12:31:01 +0530

    imagetotext.py

---
+++ imagetotext.py
@@ -0,0 +1,28 @@
+from selenium import webdriver 
+
+from PIL import Image,ImageFilter
+import pytesseract  
+
+
+
+import urllib.request
+
+#download and install pytesseract as per system's requirements
+
+##install pytesseract and pillow modules  using command pip install pytesseract and pip install pillow
+
+##edit environment variables
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+               
+# image path
+img_path ="path of the image  to be processed"
+
+###opening image
+img1=Image.open(r"C:\Users\himanig\Desktop\Python\captcha.png")
+
+
+
+
+##text will be extracted from image 
+print(pytesseract.image_to_string(img1,lang='eng'))
